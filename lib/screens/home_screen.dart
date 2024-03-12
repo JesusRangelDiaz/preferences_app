3df58338f6fef3a76d5b109a3ef4a0aab10201a7
Jesus <jesus.rangel@cceo.com.mx>
2024-03-12 10:45:49 -0600

    feat: :green_heart: Added home and settings screens. Added a hamburger menu in the side_menu widget. Defined navigation through side_menu, using main.dart as general container. added barrel files for screens and widgets folders. Added background image in assets folder for side_menu.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const String routerName='home';
+   
+  const HomeScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+      ),
+      drawer: const SideMenu(),
+      body: const Center(
+         child: Text('HomeScreen'),
+      ),
+    );
+  }
+}
